ac0afab86fa66d28a9926df92390c9fcd300a96d
woojiin <zzangwooojiin@gmail.com>
2020-06-12 22:31:46 -0400

    created a react app with layouts

---
+++ pandemic_diary/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mainScreen">
+        <div className="header"></div>
+        <div className="menuBar"></div>
+        <div className="map"></div>
+        <div className="timeLine"></div>
+      </div>
+      <div className="tweets"></div>
+      
+    </div>
+  );
+}
+
+export default App;
