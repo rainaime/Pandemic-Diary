0cfea61c508cf3818c2730aebba16126d641687c
steven tran <notsteven.tran@mail.utoronto.ca>
2020-06-24 14:26:24 -0400

    Added pop out buttons to add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Colors from './site-styles/Colors';
 import SiteHeader from './react-components/SiteHeader';
 import Menu from './react-components/Menu';
 import Maps from './react-components/Maps';
 import Tweets from './react-components/Tweets';
 import Timeline from './react-components/Timeline';
 import AddContent from './react-components/AddContent';
+import PopoutButton from './react-components/PopoutButton';
 
 class App extends React.Component {
     state = {
@@ -13,7 +15,13 @@ class App extends React.Component {
         ref: this
     }
 
-    render(){
+    render() {
+        const tempCircles = {
+            background: Colors.background,
+            width: 16,
+            height: 16,
+            borderRadius: 8
+        }
         return (
             <div className="App">
                 <SiteHeader />
@@ -21,15 +29,20 @@ class App extends React.Component {
                     <Menu f={this.handleCollapse} addContent={this.switchToAddContent} state={this.state}/>
                     {/* This div was added due to an extra wrapper div being created by
                       * the Maps component from google-maps-react. */}
-                    <div style={{ display: 'inline-block', flexGrow: 1, minWidth: '1px' }}>
+                    <div style={{ position: 'relative', display: 'inline-block', flexGrow: 1, minWidth: '1px' }}>
                         <div style={{ position: 'relative', width: '100%', height: '100%' }}>
                             {this.state.switchToAddContent ? <Maps /> : <AddContent/>}
                         </div>
+                        <PopoutButton>
+                            <span style={tempCircles}></span>
+                            <span style={tempCircles}></span>
+                            <span style={tempCircles}></span>
+                            <span style={tempCircles}></span>
+                        </PopoutButton>
                     </div>
                     <Tweets f={this.handleCollapse}/>
                 </div>
                 <Timeline />
-
             </div>
         );
     }
