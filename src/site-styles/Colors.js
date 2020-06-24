86915c389a6d7d9894cff1cafff4c84e9a4b2e49
drTricked <markdavid.sabado@mail.utoronto.ca>
2020-06-24 14:58:32 -0400

    added comments to the colors

--- src/site-styles/Colors.js
+++ src/site-styles/Colors.js
@@ -1,14 +1,14 @@
 const Colors = {
-    background :            '#003f5c',
-    backgroundDarkAccent :  '#2f4b7c',
-    backgroundLightAccent : '#a05195',
-    foreground :            '#665191',
-    foregroundAccent :      '#d45087',
-    textHighlight :         '#f95d6a',
-    textAccent1 :           '#ff7c43',
-    textAccent2 :           '#ffa600',
-    textColorDark :         'gray',
-    textColorLight :        'white'
+    background :            '#003f5c', //very dark blue
+    backgroundDarkAccent :  '#2f4b7c', //dark moderate blue
+    backgroundLightAccent : '#a05195', //dark moderate magenta
+    foreground :            '#665191', //Mostly desaturated dark violet
+    foregroundAccent :      '#d45087', //moderate pink
+    textHighlight :         '#f95d6a', //soft red
+    textAccent1 :           '#ff7c43', //light orange
+    textAccent2 :           '#ffa600', //mostly orange
+    textColorDark :         'gray',    //gray
+    textColorLight :        'white'    //white
 }
 
 export default Colors;
