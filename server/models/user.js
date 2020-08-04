af4b4cfb1fa5b21e0fc515ccb3b471b48f54fa3d
woojiin <zzangwooojiin@gmail.com>
2020-08-03 23:59:18 -0400

    starting server.js

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+// Making a Mongoose model a little differently: a Mongoose Schema
+// Allows us to add additional functionality.
+const UserSchema = new mongoose.Schema({
+	userName: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	}
+})
+
+// A static method on the document model.
+// Allows us to find a User document by comparing the hashed password
+//  to a given one, for example when logging in.
+UserSchema.statics.findByUserPassword = function(userName, password) {
+	const User = this // binds this to the User model
+
+	// First find the user by their email
+	return User.findOne({ userName: userName }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
+
+// make a model using the User schema
+const User = mongoose.model('User', UserSchema)
+module.exports = { User }
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+// starting the express server
+const app = express();
+
+// mongoose and mongo connection
+const { mongoose } = require("./db/mongoose");
+mongoose.set('useFindAndModify', false); // for some deprecation issues
+
+// import the mongoose models
+const { User } = require("./models/user");
+
+// to validate object IDs
+const { ObjectID } = require("mongodb");
+
+// body-parser: middleware for parsing HTTP JSON body into a usable object
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// express-session for managing user sessions
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create a session cookie
+app.use(
+    session({
+        secret: "oursecret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60000,
+            httpOnly: true
+        }
+    })
+);
+
+// A route to login and create a session
+app.post("/login", (req, res) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    // Use the static method on the User model to find a user
+    // by their email and password
+    User.findByUserPassword(userName, password)
+        .then(user => {
+            // Add the user's id to the session cookie.
+            // We can check later if this exists to ensure we are logged in.
+            req.session.user = user._id;
+            req.session.email = user.userName;
+            res.send({ currentUser: user.userName,
+                        password: user.password});
+        })
+        .catch(error => {
+            res.status(400).send()
+        });
+});
